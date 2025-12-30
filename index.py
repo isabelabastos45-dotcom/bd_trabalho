f9dc004213ea58877216e3b04129efa6379f63de
isabelabastos45-dotcom <isabela.bastos45@aluno.ifce.edu.br>
2025-12-30 03:13:56 -0300

    tabelas

--- index.py
+++ index.py
@@ -1,4 +1,31 @@
 import sqlite3
-conexao = sqlite3.connect('exemplo.db') #só pra salvar, precisa mudar o nome disso
 
-cursor = conexao.cursor()
+conn = sqlite3.connect('clientes.db') 
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE clientes (
+        id INT PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(150) MANDATORY,
+        idade INT,
+        cpf     VARCHAR(11) UNIQUE MANDATORY,
+        email VARCHAR(100) UNIQUE,
+        localidade VARCHAR(100),
+        data_nascimento DATE,
+        status BOOLEAN
+);
+               
+CREATE TABLE cliente_telefones (
+        id PRIMARY KEY,
+        numero VARCHAR(20),
+        tipo VARCHAR(100),
+        cliente_id FOREIGNER KEY
+               )
+""")
+
+print('Tabela criada com sucesso.')
+
+conn.close()
+
+
