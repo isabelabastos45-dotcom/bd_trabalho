28792b6b9796603348a24458c806db3db081651f
isabelabastos45-dotcom <isabela.bastos45@aluno.ifce.edu.br>
2026-01-04 01:19:04 -0300

    tabelas com os dados

--- script.py
+++ script.py
@@ -1,12 +1,22 @@
 import sqlite3
 
-conn = sqlite3.connect('clientes.db')
+conn = sqlite3.connect('gestao_clientes.db')
 cursor = conn.cursor()
 
 cursor.execute("PRAGMA foreign_keys = ON;")
 
+#detelar e recriar por conta do ON DETELE CASCADE
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS clientes (
+DROP TABLE IF EXISTS cliente_telefones;
+""")
+cursor.execute("""
+DROP TABLE IF EXISTS clientes;
+""")
+conn.commit()
+
+#criar tabelas
+cursor.execute("""
+CREATE TABLE clientes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome VARCHAR(150) NOT NULL,
     idade INTEGER,
@@ -19,15 +29,68 @@ CREATE TABLE IF NOT EXISTS clientes (
 """)
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS cliente_telefones (
+CREATE TABLE cliente_telefones (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     numero VARCHAR(20),
     tipo VARCHAR(100),
     cliente_id INTEGER,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
 );
 """)
 
 print("Tabelas criadas com sucesso")
 
+#adicionar informações em clientes
+cursor.execute("""
+INSERT INTO clientes (nome, idade, cpf, email, localidade, data_nascimento, status) 
+VALUES ('Isabela Bento Bastos', 16, '12345678915', 'isabela.bento@gmail.com', 'Fortaleza', '2009-02-01', 1);
+""")
+
+cursor.execute("""
+INSERT INTO clientes (nome, idade, cpf, email, localidade, data_nascimento, status) 
+VALUES ('Júlia Lins de Alencar', 17, '12345678815', 'julia.lins@gmail.com', 'Fortaleza', '2009-02-02', 1);
+""")
+
+cursor.execute("""
+INSERT INTO clientes (nome, idade, cpf, email, localidade, data_nascimento, status) 
+VALUES ('Letícia Ozório de Lemos', 17, '12345678715', 'leticia.ozorio@gmail.com', 'Fortaleza', '2009-02-03', 1);
+""")
+
+cursor.execute("""
+INSERT INTO clientes (nome, idade, cpf, email, localidade, data_nascimento, status) 
+VALUES ('Loren Maria Félix Lessa', 17, '12345678615', 'loren.felix@gmail.com', 'Fortaleza', '2009-02-04', 1);
+""")
+
+cursor.execute("""
+INSERT INTO clientes (nome, idade, cpf, email, localidade, data_nascimento, status) 
+VALUES ('Mauricio Ferreira', 17, '12345679915', 'mauricio.ferreira@gmail.com','Fortaleza', '2009-02-05', 1);
+""")
+
+#adicionar informações em cliente_telefones
+cursor.execute("""
+INSERT INTO cliente_telefones (numero, tipo, cliente_id) 
+VALUES ('111', 'Fixo', 1);
+""")
+cursor.execute("""
+INSERT INTO cliente_telefones (numero, tipo, cliente_id) 
+VALUES ('222', 'WhatsApp', 2);
+""")
+
+cursor.execute("""
+INSERT INTO cliente_telefones (numero, tipo, cliente_id) 
+VALUES ('333', 'Telefone', 3);
+""")
+cursor.execute("""
+INSERT INTO cliente_telefones (numero, tipo, cliente_id) 
+VALUES ('444', 'Fixo', 4);
+""")
+
+cursor.execute("""           
+INSERT INTO cliente_telefones (numero, tipo, cliente_id) 
+VALUES ('555', 'Telefone', 5);
+""")
+
+conn.commit()
 conn.close()
+
+print("Dados inseridos com sucesso")
