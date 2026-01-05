007147dec7d0c5d54864c0e0dad6a8b171b78868
MJ210624 <mauricio.ferreira@aluno.ifce.edu.br>
2026-01-04 23:46:49 -0300

    Commit ajeitando a parte de endereços

--- script.py
+++ script.py
@@ -25,6 +25,7 @@ CREATE TABLE clientes (
     idade INTEGER,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE,
+    endereco TEXT,
     localidade VARCHAR(100),
     data_nascimento DATE,
     status INTEGER NOT NULL CHECK (status IN (0,1))
@@ -45,16 +46,16 @@ print("Tabelas criadas com sucesso")
 
 #adicionar informações em clientes
 clientes = [
-    ('Isabela Bento Bastos', 16, '12345678915', 'isabela.bento@gmail.com', 'Fortaleza', '2009-02-01', 1),
-    ('Júlia Lins de Alencar', 17, '12345678815', 'julia.lins@gmail.com', 'Fortaleza', '2009-02-02', 1),
-    ('Letícia Ozório de Lemos', 17, '12345678715', 'leticia.ozorio@gmail.com', 'Fortaleza', '2009-02-03', 1),
-    ('Loren Maria Félix Lessa', 17, '12345678615', 'loren.felix@gmail.com', 'Fortaleza', '2009-02-04', 1),
-    ('Mauricio Ferreira', 17, '12345679915', 'mauricio.ferreira@gmail.com', 'Fortaleza', '2009-02-05', 1)
+    ('Isabela Bento Bastos', 16, '12345678915', 'isabela.bento@gmail.com', 'Rua Vermelha', 'Fortaleza', '2009-02-01', 1),
+    ('Júlia Lins de Alencar', 17, '12345678815', 'julia.lins@gmail.com', 'Rua Laranja', 'Fortaleza', '2009-02-02', 1),
+    ('Letícia Ozório de Lemos', 17, '12345678715', 'leticia.ozorio@gmail.com', 'Rua Amarela', 'Fortaleza', '2009-02-03', 1),
+    ('Loren Maria Félix Lessa', 17, '12345678615', 'loren.felix@gmail.com', 'Rua Verde', 'Fortaleza', '2009-02-04', 1),
+    ('Mauricio Ferreira', 17, '12345679915', 'mauricio.ferreira@gmail.com', 'Rua Azul', 'Fortaleza', '2009-02-05', 1)
 ]
 
 cursor.executemany("""
-INSERT INTO clientes (nome, idade, cpf, email, localidade, data_nascimento, status)
-VALUES (?, ?, ?, ?, ?, ?, ?)
+INSERT INTO clientes (nome, idade, cpf, email, endereco, localidade, data_nascimento, status)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)
 """, clientes)
 
 
